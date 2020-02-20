ed40157dfcaece9fc758f5fb028a47fd8aa23b60
banminseok <30819583+banminseok@users.noreply.github.com>
2020-02-20 15:49:43 +0900

    Update App.js

--- hello-react/src/App.js
+++ hello-react/src/App.js
@@ -1,31 +1,8 @@
-import React, { Component } from "react";
-import ScrollBox from "./ScrollBox";
+import React from "react";
+import IterationSample from "./IterationSample";
 
-class App extends Component {
-  render() {
-    return (
-      <div>
-        <ScrollBox
-          ref={ref => {
-            this.scrollBox = ref;
-          }}
-        />
-        <button onClick={() => this.scrollBox.scrollToBottom()}>
-          맨밑으로
-        </button>
-      </div>
-    );
-  }
-}
+const App = () => {
+  return <IterationSample />;
+};
 
 export default App;
-
-/*import React from "react";
-
-
-function App() {
-  return <ValidationSample />;
-}
-
-export default App;
-*/
