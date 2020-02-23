f19ef64bc8b8077f83d5aa3eb833ec5028597cfb
banminseok <30819583+banminseok@users.noreply.github.com>
2020-02-23 16:26:39 +0900

    Update App.js

--- hello-react/src/App.js
+++ hello-react/src/App.js
@@ -1,8 +1,31 @@
-import React from "react";
-import IterationSample from "./IterationSample";
+import React, { Component } from "react";
+import LifeCycleSample from "./LifeCycleSample";
+import ErrorBoundary from "./ErrorBoundary";
 
-const App = () => {
-  return <IterationSample />;
-};
+function getRandomColor() {
+  return "#" + Math.floor(Math.random() * 16777215).toString(16);
+}
+class App extends Component {
+  state = {
+    color: "#000000",
+    number: null
+  };
+
+  handleClick = () => {
+    this.setState({
+      color: getRandomColor()
+    });
+  };
+  render() {
+    return (
+      <div>
+        <button onClick={this.handleClick}>랜덤색상</button>
+        <ErrorBoundary>
+          <LifeCycleSample color={this.state.color} />
+        </ErrorBoundary>
+      </div>
+    );
+  }
+}
 
 export default App;
