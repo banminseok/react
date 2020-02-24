1ae2a54a2743e2fbff63bb4a9b6730795055ef38
banminseok <30819583+banminseok@users.noreply.github.com>
2020-02-24 18:01:00 +0900

    Create Info.js

---
+++ hooks-tutorial/src/Info.js
@@ -0,0 +1,47 @@
+import React, { useState, useEffect, useReducer } from "react";
+
+function reducer(state, action) {
+  console.log("state", state);
+  return {
+    ...state,
+    [action.name]: action.value
+  };
+}
+const Info = () => {
+  const [state, dispatch] = useReducer(reducer, {
+    name: "",
+    nickname: ""
+  });
+  const { name, nickname } = state;
+
+  useEffect(() => {
+    console.log("effect.");
+    console.log(name);
+    return () => {
+      console.log("cleanup.");
+      console.log(name);
+    };
+  });
+
+  const onChange = e => {
+    dispatch(e.target);
+  };
+  return (
+    <div>
+      <div>
+        <input name="name" value={name} onChange={onChange} />
+        <input name="nickname" value={nickname} onChange={onChange} />
+      </div>
+      <div>
+        <div>
+          <b>이름 :</b> {name}
+        </div>
+        <div>
+          <b>닉네임 :</b> {nickname}
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Info;
